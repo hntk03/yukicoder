5142db033921b3a4d98267204862196e76252417
hntk03 <hntk03@gmail.com>
2019-04-14 19:33:01 +0900

    a

---
+++ No809.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<string> VS;
+
+
+//container util
+#define SORT(c) sort((c).begin(),(c).end())
+
+//repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
+
+
+int main(void){
+
+	int C; cin >> C;
+
+	cout << 1 << " " << C << endl;
+	return 0;
+
+}
